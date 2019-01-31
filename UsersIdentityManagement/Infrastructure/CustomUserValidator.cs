7ac04dd0b6534defe6a996e4016eab1dc448a199
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-02-01 01:17:49 +0200

    Build CustomUserValidator extending UserValidator class

---
+++ UsersIdentityManagement/Infrastructure/CustomUserValidator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsersIdentityManagement.Models.AppUserModels;
+
+namespace UsersIdentityManagement.Infrastructure
+{
+    public class CustomUserValidator : UserValidator<AppUser>
+    {
+        public override async Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager, AppUser user)
+        {
+            IdentityResult result = await base.ValidateAsync(manager, user);
+            List<IdentityError> errors = result.Succeeded ?
+                new List<IdentityError>() : result.Errors.ToList();
+            errors.ForEach(e => {
+                if (e.Code == "InvalidUserName")
+                    e.Description = $"Invalid name '{user.UserName}', can only contain lower case letters.";
+            });
+
+            if (!user.Email.EndsWith("@example.com"))
+            {
+                errors.Add(new IdentityError()
+                {
+                    Code = "InvalidDomainName",
+                    Description = "Only 'example.com' domain name is allowed"
+                });
+            }
+
+            return errors.Count == 0 ? 
+                IdentityResult.Success : IdentityResult.Failed(errors.ToArray());
+        }
+    }
+}
