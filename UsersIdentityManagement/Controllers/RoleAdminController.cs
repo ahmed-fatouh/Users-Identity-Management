dcb1f7b0f61ebb55c2be835ea05343d17d8b645e
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-02-05 03:33:39 +0200

    Create RoleAdminController with Index action and view

---
+++ UsersIdentityManagement/Controllers/RoleAdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace UsersIdentityManagement.Controllers
+{
+    public class RoleAdminController : Controller
+    {
+        private RoleManager<IdentityRole> roleManager;
+
+        public RoleAdminController(RoleManager<IdentityRole> roleMgr)
+        {
+            roleManager = roleMgr;
+        }
+
+        public ViewResult Index() => View(roleManager.Roles);
+
+    }
+}
