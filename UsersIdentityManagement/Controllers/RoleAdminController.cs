b7c472c95d1797fcba10b9a65298f4f3498b374a
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-02-05 04:58:23 +0200

    Add Create and Delete actions to RoleAdminController with Create view

--- UsersIdentityManagement/Controllers/RoleAdminController.cs
+++ UsersIdentityManagement/Controllers/RoleAdminController.cs
@@ -19,5 +19,60 @@ namespace UsersIdentityManagement.Controllers
 
         public ViewResult Index() => View(roleManager.Roles);
 
+        [HttpGet]
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Required]string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                    return RedirectToAction(nameof(Index));
+                else
+                    AddModelStateErrors(result);
+            }
+            return View(name);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IActionResult actionResult = await CheckIdAndRoleAsync(id);
+            if (actionResult != null)
+                return actionResult;
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            IdentityResult result = await roleManager.DeleteAsync(role);
+            if (result.Succeeded)
+                return RedirectToAction(nameof(Index));
+            else
+            {
+                string errors = "Can't Delete Role: \n";
+                result.Errors.ToList().ForEach(e => errors += e + "\n");
+                return Content(errors);
+            }
+        }
+
+        #region GenericHelperMethods
+        private void AddModelStateErrors(IdentityResult failedIdentityResult)
+        {
+            foreach (IdentityError error in failedIdentityResult.Errors)
+                ModelState.AddModelError("", error.Description);
+        }
+
+        //Checks if a non-null Id value is sent and if a role with this id is found.
+        //Otherwise, it returns null.
+        private async Task<IActionResult> CheckIdAndRoleAsync(string id)
+        {
+            IdentityRole role = null;
+            if (id == null)
+                return BadRequest();
+            role = await roleManager.FindByIdAsync(id);
+            if (role == null)
+                return NotFound();
+            return null;
+        }
+        #endregion
     }
 }
