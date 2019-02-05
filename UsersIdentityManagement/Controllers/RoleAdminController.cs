4e3853bf1a85c5a6a40dc7b5d2118c7be5021b1d
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-02-05 23:46:38 +0200

    Add Edit logic to RoleAdminController
    This includes adding RoleEdit &RoleModification view models, Edit action and Edit view.

--- UsersIdentityManagement/Controllers/RoleAdminController.cs
+++ UsersIdentityManagement/Controllers/RoleAdminController.cs
@@ -5,16 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
+using UsersIdentityManagement.Models.AppUserModels;
+using UsersIdentityManagement.Models;
 
 namespace UsersIdentityManagement.Controllers
 {
     public class RoleAdminController : Controller
     {
         private RoleManager<IdentityRole> roleManager;
+        private UserManager<AppUser> userManager;
 
-        public RoleAdminController(RoleManager<IdentityRole> roleMgr)
+        public RoleAdminController(RoleManager<IdentityRole> roleMgr, UserManager<AppUser> usrMgr)
         {
             roleManager = roleMgr;
+            userManager = usrMgr;
         }
 
         public ViewResult Index() => View(roleManager.Roles);
@@ -52,6 +56,74 @@ namespace UsersIdentityManagement.Controllers
                 result.Errors.ToList().ForEach(e => errors += e + "\n");
                 return Content(errors);
             }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            IActionResult actionResult = await CheckIdAndRoleAsync(id);
+            if (actionResult != null)
+                return actionResult;
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            RoleEditModel model = new RoleEditModel() {
+                Role = role,
+                Members = new List<AppUser>(),
+                NonMembers = new List<AppUser>()
+            };
+            
+            foreach (AppUser user in userManager.Users)
+            {
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                    model.Members = model.Members.Append(user);
+                else
+                    model.NonMembers = model.NonMembers.Append(user);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(RoleModificationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IActionResult actionResult = await CheckIdAndRoleAsync(model.RoleId);
+                if (actionResult != null)
+                    return actionResult;
+                IdentityRole role = await roleManager.FindByIdAsync(model.RoleId);
+                if (role.Name != model.RoleName)
+                    return BadRequest();
+
+                IdentityResult result;
+                foreach (string id in model.IdsToAdd ?? new string[] { })
+                {
+                    AppUser user = await userManager.FindByIdAsync(id);
+                    if (user != null)
+                    {
+                        result = await userManager.AddToRoleAsync(user, model.RoleName);
+                        if (!result.Succeeded)
+                            AddModelStateErrors(result);
+                    }
+                    else
+                        ModelState.AddModelError("", $"User '{user.UserName}' not found");
+                }
+
+                foreach (string id in model.IdsToDelete ?? new string[] { })
+                {
+                    AppUser user = await userManager.FindByIdAsync(id);
+                    if (user != null)
+                    {
+                        result = await userManager.RemoveFromRoleAsync(user, model.RoleName);
+                        if (!result.Succeeded)
+                            AddModelStateErrors(result);
+                    }
+                    else
+                        ModelState.AddModelError("", $"User '{user.UserName}' not found");
+                }
+
+                if (ModelState.ErrorCount == 0)
+                    return RedirectToAction(nameof(Index));
+            }
+            return await Edit(model.RoleId);
         }
 
         #region GenericHelperMethods
--- UsersIdentityManagement/Models/UserViewModels.cs
+++ UsersIdentityManagement/Models/UserViewModels.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using UsersIdentityManagement.Models.AppUserModels;
 
 namespace UsersIdentityManagement.Models
 {
@@ -46,4 +48,20 @@ namespace UsersIdentityManagement.Models
         public string Password { get; set; }
     }
 
+    public class RoleEditModel
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<AppUser> Members { get; set; }
+        public IEnumerable<AppUser> NonMembers { get; set; }
+    }
+
+    public class RoleModificationModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+        [Required]
+        public string RoleId { get; set; }
+        public string[] IdsToAdd { get; set; }
+        public string[] IdsToDelete { get; set; }
+    }
 }
